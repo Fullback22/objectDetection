4fafd8727319d36a570d4a80331306c3fd3fdd26
Fullback22 <andrew-sergeenko@mail.ru>
2022-08-30 21:46:32 +0300

    some changes in working with the camera

--- objectDetection/objectDetection.py
+++ objectDetection/objectDetection.py
@@ -1,14 +1,10 @@
-import cv2
+import cv2 as cv
+import numpy as np
 from gui_buttons import Buttons
 
-# Initialize Buttons
-button = Buttons()
-colors = button.colors
-
-
 # Opencv DNN
-net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
-model = cv2.dnn_DetectionModel(net)
+net = cv.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+model = cv.dnn_DetectionModel(net)
 model.setInputParams(size=(320, 320), scale=1/255)
 
 # Load class lists
@@ -18,39 +14,57 @@ with open("dnn_model/classes.txt", "r") as file_object:
         class_name = class_name.strip()
         classes.append(class_name)
 
-# Initialize camera
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
 # FULL HD 1920 x 1080
+height = 720
+width = 1280
+frame = np.zeros((height,width,3), np.uint8)
+messageColor = (255,255,255)
+# Initialize camera
+cap = cv.VideoCapture(0)
 
 # Create window
-cv2.namedWindow("Frame")
+cv.namedWindow("Frame")
 
+if not cap.isOpened():
+     message = "Webcam not connected"
+     x = int(width / 4)
+     y = int(height / 2)
+     cv.putText(frame, message, (x, y), cv.FONT_HERSHEY_PLAIN, 3, messageColor, 2)
+     cv.imshow("Frame", frame)
 
-while True:
-    # Get frames
-    ret, frame = cap.read()
+else:
+     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+     
+     isCameraConnected = True
+     while isCameraConnected:
+          # Get frames
+          isCameraConnected, frame = cap.read()
+          if isCameraConnected:
 
-    # Get active buttons list
-    active_buttons = button.active_buttons_list()
-    #print("Active buttons", active_buttons)
+               # Object Detection
+               (class_ids, scores, bboxes) = model.detect(frame, confThreshold=0.3, nmsThreshold=.4)
+               for class_id, score, bbox in zip(class_ids, scores, bboxes):
+                    (x, y, w, h) = bbox
+                    class_name = classes[class_id]
+                    colorBottle = (255,100,200)
 
-    # Object Detection
-    (class_ids, scores, bboxes) = model.detect(frame, confThreshold=0.3, nmsThreshold=.4)
-    for class_id, score, bbox in zip(class_ids, scores, bboxes):
-        (x, y, w, h) = bbox
-        class_name = classes[class_id]
-        color = colors[class_id]
+                    if class_name == "bottle":
+                         cv.putText(frame, class_name, (x, y - 10), cv.FONT_HERSHEY_PLAIN, 3, colorBottle, 2)
+                         cv.rectangle(frame, (x, y), (x + w, y + h), colorBottle, 3)
 
-        if class_name == "bottle":
-            cv2.putText(frame, class_name, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 3, color, 2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 3)
+          else:
+               message = "Webcam disconnect"
+               x = int(width / 4)
+               y = int(height / 2)
+               frame = np.zeros((height,width,3), np.uint8)
+               cv.putText(frame, message, (x, y), cv.FONT_HERSHEY_PLAIN, 2, messageColor, 2)
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+          cv.imshow("Frame", frame)
+          key = cv.waitKey(1)
 
-cap.release()
-cv2.destroyAllWindows()
+     cap.release()
+
+cv.waitKey()
+cv.destroyAllWindows()
